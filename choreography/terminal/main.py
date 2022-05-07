dfa25ea32c1d0d0e9dde5e3bc23929c5c807462a
Tomasz Wiśniewski <62062763+Wisnia44@users.noreply.github.com>
2022-05-07 14:44:29 +0100

    Implementing store entering process for choreography (#3)

--- choreography/terminal/main.py
+++ choreography/terminal/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+import logging
+import os
+
+import httpx
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from shared.models import Card
 
 app = FastAPI()
+logger = logging.getLogger()
+logger.setLevel(os.getenv("LOGGER_LEVEL", "INFO"))
 
 
-@app.get("/health")
-def health():
-    return {"Hello": "World"}
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def health():
+    return {}
+
+
+@app.post("/card_scanned", response_model=Card)
+async def card_scanned(card: Card):
+    logger.warning("Card scanned detected")
+    logger.warning("Initializing entering process")
+    logger.warning("Sending request to CRM for card_scanned")
+    async with httpx.AsyncClient() as client:
+        await client.post(
+            "http://crm_choreography:8002/card_scanned",
+            json={"card_token": card.card_token},
+        )
+    return JSONResponse(status_code=status.HTTP_200_OK, content=card.reprJSON())
